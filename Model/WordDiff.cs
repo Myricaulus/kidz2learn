448ca7bd5258c068db82b2f6ef0c3f1342f2c273
Myricaulus <myric.log@gmx.de>
2026-07-18 12:58:29 +0200

    New Popup for to find errors

---
+++ Model/WordDiff.cs
@@ -0,0 +1,135 @@
+namespace Kidz2Learn.Pages.SilbenChallenge;
+
+/// <summary>
+/// Berechnet ein Buchstaben-Alignment (Wagner-Fischer / Levenshtein) zwischen dem
+/// korrekten Wort und einer falsch gewählten Antwort, um daraus abzuleiten,
+/// - welche Buchstaben im falschen Wort markiert ("falsch") werden müssen
+///   (Ersetzungen + überzählige Buchstaben), und
+/// - an welchen Lücken-Positionen im falschen Wort ein Buchstabe fehlt
+///   (inkl. des jeweils erwarteten Buchstabens).
+/// </summary>
+public static class WordDiff
+{
+    public enum OpType
+    {
+        Match,
+        Substitute,
+        Delete,
+        Insert
+    }
+
+    public readonly record struct DiffOp(OpType Type, char? CorrectChar, char? WrongChar);
+
+    public readonly record struct DiffRequirements(
+        HashSet<int> RequiredMarks,
+        Dictionary<int, char> RequiredSubstitutions,
+        Dictionary<int, char> RequiredGaps);
+
+    /// <summary>
+    /// Liefert die Alignment-Operationen zwischen <paramref name="correct"/> und
+    /// <paramref name="wrong"/>, in Reihenfolge des falschen Wortes.
+    /// </summary>
+    public static List<DiffOp> Align(string correct, string wrong)
+    {
+        var c = correct;
+        var w = wrong;
+        var n = c.Length;
+        var m = w.Length;
+
+        var dp = new int[n + 1, m + 1];
+        for (var i = 0; i <= n; i++) dp[i, 0] = i;
+        for (var j = 0; j <= m; j++) dp[0, j] = j;
+
+        for (var i = 1; i <= n; i++)
+        for (var j = 1; j <= m; j++)
+        {
+            if (char.ToLowerInvariant(c[i - 1]) == char.ToLowerInvariant(w[j - 1]))
+            {
+                dp[i, j] = dp[i - 1, j - 1];
+            }
+            else
+            {
+                var sub = dp[i - 1, j - 1] + 1;
+                var del = dp[i - 1, j] + 1; // Buchstabe aus "correct" fehlt in "wrong" -> Insert-Op
+                var ins = dp[i, j - 1] + 1; // Buchstabe in "wrong" ist überzählig -> Delete-Op
+                dp[i, j] = Math.Min(sub, Math.Min(del, ins));
+            }
+        }
+
+        // Backtracking vom Ende her, danach umdrehen
+        var ops = new List<DiffOp>();
+        var a = n;
+        var b = m;
+        while (a > 0 || b > 0)
+        {
+            if (a > 0 && b > 0 && char.ToLowerInvariant(c[a - 1]) == char.ToLowerInvariant(w[b - 1]))
+            {
+                ops.Add(new DiffOp(OpType.Match, c[a - 1], w[b - 1]));
+                a--;
+                b--;
+            }
+            else if (a > 0 && b > 0 && dp[a, b] == dp[a - 1, b - 1] + 1)
+            {
+                ops.Add(new DiffOp(OpType.Substitute, c[a - 1], w[b - 1]));
+                a--;
+                b--;
+            }
+            else if (b > 0 && dp[a, b] == dp[a, b - 1] + 1)
+            {
+                // Buchstabe existiert in "wrong", aber nicht im korrekten Wort -> überzählig
+                ops.Add(new DiffOp(OpType.Delete, null, w[b - 1]));
+                b--;
+            }
+            else
+            {
+                // Buchstabe existiert im korrekten Wort, aber nicht in "wrong" -> fehlt (Lücke)
+                ops.Add(new DiffOp(OpType.Insert, c[a - 1], null));
+                a--;
+            }
+        }
+
+        ops.Reverse();
+        return ops;
+    }
+
+    /// <summary>
+    /// Wandelt die Alignment-Operationen in die für das Popup benötigten
+    /// "Musterlösungen" um:
+    /// - RequiredMarks: Indizes überzähliger Buchstaben im falschen Wort (nur markieren, nichts einzutippen).
+    /// - RequiredSubstitutions: Indizes von inhaltlich falschen Buchstaben, mit dem jeweils
+    ///   korrekten Ersatzbuchstaben, der dort eingetippt werden muss.
+    /// - RequiredGaps: Lücken-Positionen mit fehlendem Buchstaben (wie gehabt).
+    /// Lücken-Index k bedeutet "vor dem k-ten Buchstaben des falschen Wortes"
+    /// (k == Länge des falschen Wortes => Lücke ganz am Ende).
+    /// </summary>
+    public static DiffRequirements BuildRequirements(string correct, string wrong)
+    {
+        var ops = Align(correct, wrong);
+
+        var marks = new HashSet<int>();
+        var substitutions = new Dictionary<int, char>();
+        var gaps = new Dictionary<int, char>();
+        var wrongIndex = 0;
+
+        foreach (var op in ops)
+            switch (op.Type)
+            {
+                case OpType.Match:
+                    wrongIndex++;
+                    break;
+                case OpType.Substitute:
+                    substitutions[wrongIndex] = op.CorrectChar!.Value;
+                    wrongIndex++;
+                    break;
+                case OpType.Delete:
+                    marks.Add(wrongIndex);
+                    wrongIndex++;
+                    break;
+                case OpType.Insert:
+                    gaps[wrongIndex] = op.CorrectChar!.Value;
+                    break;
+            }
+
+        return new DiffRequirements(marks, substitutions, gaps);
+    }
+}
